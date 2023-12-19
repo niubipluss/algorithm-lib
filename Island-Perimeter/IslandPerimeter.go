5f010b8fb7cf89dbb21708a4092f1bc0f142c1c3
ydhmkpsryk321@gmail.com <ydhmkpsryk321@gmail.com>
2023-12-19 16:10:30 +0800

    add-IslandPerimeter.go-2023-12-19 16:10:28

---
+++ Island-Perimeter/IslandPerimeter.go
@@ -0,0 +1,27 @@
+// FILE: Island-Perimeter/IslandPerimeter.go
+// AUTH: tO28n1X75@gmail.com
+// DES: add-2023-12-19 16:10:28
+package leetcode
+
+func islandPerimeter(grid [][]int) int {
+	counter := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 1 {
+				if i-1 < 0 || grid[i-1][j] == 0 {
+					counter++
+				}
+				if i+1 >= len(grid) || grid[i+1][j] == 0 {
+					counter++
+				}
+				if j-1 < 0 || grid[i][j-1] == 0 {
+					counter++
+				}
+				if j+1 >= len(grid[0]) || grid[i][j+1] == 0 {
+					counter++
+				}
+			}
+		}
+	}
+	return counter
+}
