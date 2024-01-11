26d1e36c56430666a88ebe16f06cd4ccffeb1b31
ydhmkpsryk321@gmail.com <ydhmkpsryk321@gmail.com>
2024-01-11 14:54:48 +0800

    add-SumofRootToLeafBinaryNumbers.go-2024-01-11

---
+++ Sum-of-Root-To-Leaf-Binary-Numbers/SumofRootToLeafBinaryNumbers.go
@@ -0,0 +1,33 @@
+// FILE: Sum-of-Root-To-Leaf-Binary-Numbers/SumofRootToLeafBinaryNumbers.go
+// AUTH: ydhmkpsryk321@gmail.com
+// DES: 2024-01-11 14:54:46
+package leetcode
+
+import "github.com/halfrost/LeetCode-Go/structures"
+
+// TreeNode define
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumRootToLeaf(root *TreeNode) int {
+	var dfs func(*TreeNode, int) int
+	dfs = func(node *TreeNode, sum int) int {
+		if node == nil {
+			return 0
+		}
+		sum = sum<<1 | node.Val
+		// 上一行也可以写作 sum = sum*2 + node.Val
+		if node.Left == nil && node.Right == nil {
+			return sum
+		}
+		return dfs(node.Left, sum) + dfs(node.Right, sum)
+	}
+	return dfs(root, 0)
+}
