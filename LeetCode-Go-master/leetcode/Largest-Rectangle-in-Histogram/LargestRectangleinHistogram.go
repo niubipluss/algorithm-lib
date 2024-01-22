8b0d7975702129ff326eafd540ee8ed49d08e5a7
ydhmkpsryk321@gmail.com <ydhmkpsryk321@gmail.com>
2024-01-22 11:09:10 +0800

    add-LargestRectangleinHistogram.go-2024-01-22

---
+++ LeetCode-Go-master/leetcode/Largest-Rectangle-in-Histogram/LargestRectangleinHistogram.go
@@ -0,0 +1,35 @@
+// FILE: LeetCode-Go-master/leetcode/Largest-Rectangle-in-Histogram/LargestRectangleinHistogram.go
+// AUTH: ydhmkpsryk321@gmail.com
+// DES: 2024-01-22 11:09:09
+package leetcode
+
+func largestRectangleArea(heights []int) int {
+	maxArea := 0
+	n := len(heights) + 2
+	// Add a sentry at the beginning and the end
+	getHeight := func(i int) int {
+		if i == 0 || n-1 == i {
+			return 0
+		}
+		return heights[i-1]
+	}
+	st := make([]int, 0, n/2)
+	for i := 0; i < n; i++ {
+		for len(st) > 0 && getHeight(st[len(st)-1]) > getHeight(i) {
+			// pop stack
+			idx := st[len(st)-1]
+			st = st[:len(st)-1]
+			maxArea = max(maxArea, getHeight(idx)*(i-st[len(st)-1]-1))
+		}
+		// push stack
+		st = append(st, i)
+	}
+	return maxArea
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
