feb4bcd533c214983560ced95d2319839b41171a
ydhmkpsryk321@gmail.com <ydhmkpsryk321@gmail.com>
2024-01-24 10:07:16 +0800

    add-SearchSuggestionsSystem.go-2024-01-24

---
+++ Search-Suggestions-System/SearchSuggestionsSystem.go
@@ -0,0 +1,25 @@
+// FILE: Search-Suggestions-System/SearchSuggestionsSystem.go
+// AUTH: ydhmkpsryk321@gmail.com
+// DES: 2024-01-24 10:07:14
+package leetcode
+
+import (
+	"sort"
+)
+
+func suggestedProducts(products []string, searchWord string) [][]string {
+	sort.Strings(products)
+	searchWordBytes, result := []byte(searchWord), make([][]string, 0, len(searchWord))
+	for i := 1; i <= len(searchWord); i++ {
+		searchWordBytes[i-1]++
+		products = products[:sort.SearchStrings(products, string(searchWordBytes[:i]))]
+		searchWordBytes[i-1]--
+		products = products[sort.SearchStrings(products, searchWord[:i]):]
+		if len(products) > 3 {
+			result = append(result, products[:3])
+		} else {
+			result = append(result, products)
+		}
+	}
+	return result
+}
