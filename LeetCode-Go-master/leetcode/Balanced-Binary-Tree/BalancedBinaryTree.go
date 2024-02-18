2f5af0125d4b356238848a475ca0afad43dc361e
ydhmkpsryk321@gmail.com <ydhmkpsryk321@gmail.com>
2024-02-18 17:13:18 +0800

    add-BalancedBinaryTree.go-2024-02-18

---
+++ LeetCode-Go-master/leetcode/Balanced-Binary-Tree/BalancedBinaryTree.go
@@ -0,0 +1,50 @@
+// FILE: LeetCode-Go-master/leetcode/Balanced-Binary-Tree/BalancedBinaryTree.go
+// AUTH: ydhmkpsryk321@gmail.com
+// DES: 2024-02-18 17:13:16
+package leetcode
+
+import (
+	"github.com/halfrost/LeetCode-Go/structures"
+)
+
+// TreeNode define
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	leftHight := depth(root.Left)
+	rightHight := depth(root.Right)
+	return abs(leftHight-rightHight) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func depth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(depth(root.Left), depth(root.Right)) + 1
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
